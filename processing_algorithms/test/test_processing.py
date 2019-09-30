3c90e0c2a3a9cc7f7fab6a7e50f22b203b2ab487
Etienne Trimaille <gustrimaille@yahoo.fr>
2019-09-30 18:36:12 +0200

    WIP travis

---
+++ processing_algorithms/test/test_processing.py
@@ -0,0 +1,25 @@
+from qgis.testing import unittest, start_app
+from qgis.core import QgsApplication, QgsCoordinateReferenceSystem
+
+import processing
+
+from ..provider import Provider
+
+
+class ProcessingTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        start_app()
+        QgsApplication.processingRegistry().addProvider(Provider())
+        processing.Processing.initialize()
+
+    def test_processing(self):
+        for alg in QgsApplication.processingRegistry().algorithms():
+            print(alg.id(), "->", alg.displayName())
+
+    def test_create_geopackage(self):
+        params = {
+            'FILE_GPKG': '/tmp/processing_605a277cecad4784a82aca33d9c21c03/c06e4a949d014c95a76bcd38a93e7e3d/FILE_GPKG.file',
+            'CRS': QgsCoordinateReferenceSystem('EPSG:2154')}
+        result = processing.run(
+            'drain_sewer_visual_inspection:create_geopackage', params)

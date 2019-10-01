dfdd4dc861d86a5cb6266c36f4df1997ee64743f
Etienne Trimaille <gustrimaille@yahoo.fr>
2019-10-01 18:06:17 +0200

    add import manholes layer algorithm

--- processing_algorithms/test/test_processing.py
+++ processing_algorithms/test/test_processing.py
@@ -3,7 +3,7 @@ import os
 
 from qgis.PyQt.QtCore import QCoreApplication, QSettings
 from qgis.testing import unittest, start_app
-from qgis.core import QgsApplication, QgsCoordinateReferenceSystem
+from qgis.core import QgsApplication, QgsCoordinateReferenceSystem, QgsVectorLayer
 
 import sys
 if not hasattr(sys, 'argv'):
@@ -17,6 +17,7 @@ except ImportError:
     from qgis import processing
 
 from ..provider import Provider
+from ...qgis_plugin_tools.resources import plugin_test_data_path
 
 
 class ProcessingTest(unittest.TestCase):
@@ -37,11 +38,20 @@ class ProcessingTest(unittest.TestCase):
         QgsApplication.processingRegistry().addProvider(self.provider)
         processing.Processing.initialize()
 
+    def test_layer(self):
+        """Quick test for the layer."""
+        layer_path = plugin_test_data_path('manholes_to_import.geojson')
+        layer = QgsVectorLayer(layer_path, 'test', 'ogr')
+        self.assertTrue(layer.isValid(), '{} is not valid'.format(layer_path))
+
     def test_create_geopackage(self):
         """Test the workflow about DSVI."""
-        path = os.path.join(tempfile.mkdtemp(), 'test.gpkg')
+        # Geopackage for testing
+        geopackage_path = os.path.join(tempfile.mkdtemp(), 'test.gpkg')
+
+        # Create geopackage
         params = {
-            'FILE_GPKG': path,
+            'FILE_GPKG': geopackage_path,
             'CRS': QgsCoordinateReferenceSystem('EPSG:2154')}
         result = processing.run(
             'drain_sewer_visual_inspection:create_geopackage', params)
@@ -50,14 +60,26 @@ class ProcessingTest(unittest.TestCase):
         for layer in result['OUTPUT_LAYERS']:
             self.assertTrue(layer.isValid())
 
+        # Setting up the project
         params = {
-            'TABLE_FICHIER': '{}|layername=file'.format(path),
-            'TABLE_TRONCON': '{}|layername=troncon'.format(path),
-            'TABLE_OBSERVATIONS': '{}|layername=obs'.format(path),
-            'TABLE_REGARD': '{}|layername=regard'.format(path),
-            'COUCHE_GEOM_REGARD': '{}|layername=geom_regard'.format(path),
-            'COUCHE_GEOM_TRONCON': '{}|layername=geom_troncon'.format(path),
-            'COUCHE_GEOM_OBSERVATION': '{}|layername=geom_obs'.format(path),
-            'VIEW_REGARD_GEOLOCALIZED': '{}|layername=view_regard_geolocalized'.format(path),
+            'TABLE_FICHIER': '{}|layername=file'.format(geopackage_path),
+            'TABLE_TRONCON': '{}|layername=troncon'.format(geopackage_path),
+            'TABLE_OBSERVATIONS': '{}|layername=obs'.format(geopackage_path),
+            'TABLE_REGARD': '{}|layername=regard'.format(geopackage_path),
+            'COUCHE_GEOM_REGARD': '{}|layername=geom_regard'.format(geopackage_path),
+            'COUCHE_GEOM_TRONCON': '{}|layername=geom_troncon'.format(geopackage_path),
+            'COUCHE_GEOM_OBSERVATION': '{}|layername=geom_obs'.format(geopackage_path),
+            'VIEW_REGARD_GEOLOCALIZED': '{}|layername=view_regard_geolocalized'.format(geopackage_path),
         }
-        processing.run('drain_sewer_visual_inspection:config_dsvi_project', params)
+        result = processing.run('drain_sewer_visual_inspection:config_dsvi_project', params)
+
+        # Import regard into geopackage
+        layer_path = plugin_test_data_path('manholes_to_import.geojson')
+        layer = QgsVectorLayer(layer_path, 'test', 'ogr')
+        self.assertTrue(layer.isValid())
+        params = {
+            'COUCHE_A_IMPORTER': layer,
+            'CHAMP_NOM_REGARD': 'name',
+            'COUCHE_GEOM_REGARD': '{}|layername=geom_regard'.format(geopackage_path)
+        }
+        result = processing.run("drain_sewer_visual_inspection:import_geom_regard", params)
